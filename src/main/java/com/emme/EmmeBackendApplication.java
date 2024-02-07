adeb6a3e0d8b0ba8360d8e51f3fd19db288722c1
BulletJim <gianmarco.campania@gmail.com>
2024-02-07 02:23:43 +0100

    added base project structure

---
+++ src/main/java/com/emme/EmmeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmmeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmmeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emme/EmmeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmmeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
